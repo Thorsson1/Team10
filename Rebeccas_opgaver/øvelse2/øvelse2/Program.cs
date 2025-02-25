256c337bd98f9f9fd3a4258d4e85d9d82beacd83
Rebecca P <rebecca_pedersen@msn.com>
2025-02-25 14:49:52 +0100

    test

---
+++ Rebeccas_opgaver/øvelse2/øvelse2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace øvelse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vælg en mulighed:");
+            Console.WriteLine("1: Celsius til Fahrenheit");
+            Console.WriteLine("2: Fahrenheit til Celsius");
+
+            int choice;
+            bool isValid = int.TryParse(Console.ReadLine(), out choice);
+
+            if (!isValid || (choice != 1 && choice != 2))
+            {
+                Console.WriteLine("Ugyldigt valg.");
+                return;
+            }
+
+            Console.Write("Indtast temperatur: ");
+            double temperature;
+            isValid = double.TryParse(Console.ReadLine(), out temperature);
+
+            if (!isValid)
+            {
+                Console.WriteLine("Ugyldig temperatur.");
+                return;
+            }
+
+            if (choice == 1)
+            {
+                double fahrenheit = (temperature * 9 / 5) + 32;
+                Console.WriteLine($"Celsius: {temperature}°C = Fahrenheit: {fahrenheit:F2}°F");// F2 viser resultatet med 2 decimaler
+            }
+            else
+            {
+                double celsius = (temperature - 32) * 5 / 9;
+
+            }
+        }
+    }
+}
+
+    
+
---
+++ Rebeccas_opgaver/øvelser/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace øvelser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            double height = 1.75;
+            double weight = 70.5;
+            char initial = 'A';
+            string name = "Alice";
+            bool isStudent = true;
+            Console.WriteLine($"Age: {age}, Height: {height}, Weight: {weight}, Initial: {initial}, Name: {name}, Is Student: {isStudent}");
+
+
+        }
+
+
+        }
+    }
+
+
