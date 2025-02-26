ad5e3fc8ae614693c34fbc771a988c0983706137
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-02-26 16:29:13 +0100

    RudRettetOpgave7

--- Ruds_Opgave/Rudopgave7/Program.cs
+++ Ruds_Opgave/Rudopgave7/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Lad os tjekke, om du er født i et skudår! \nIndtast i tal, hvad år du er født i: ");
+            Console.WriteLine("Velkommen! Læg venligst mærke til, at dette både er opgave 7 + opgave 9");
+            //Opgave 7
+            Console.Write("Lad os starte med at tjekke, om et tal er positivt, negativt eller lig med 0." +
+                "\nIndsæt et tal: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int number = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+
+            if (number < 0)
+            {
+                Console.WriteLine("Tallet du har indtastet er negativt");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("Tallet du har indtastet er lig med nul");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("Tallet du har indtastet er positivt");
+            }
+            else
+            {
+                Console.WriteLine("Du har indtastet noget ugyldigt");
+            }
+
+            //Opgave 9
+            Console.Write("\nLad os nu tjekke, om du er født i et skudår! \nIndtast i tal, hvad år du er født i: ");
             Console.ForegroundColor = ConsoleColor.Green;
             int year = int.Parse(Console.ReadLine());
             Console.ResetColor();
--- Ruds_Opgave/Rudopgave7/obj/Debug/net8.0/Rudopgave7.AssemblyInfo.cs
+++ Ruds_Opgave/Rudopgave7/obj/Debug/net8.0/Rudopgave7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rudopgave7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011a06ecc1dfa864217bcc17ad38a0ec43c6deba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd37349271a0631336e4d65762bd6cad177848d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rudopgave7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rudopgave7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
