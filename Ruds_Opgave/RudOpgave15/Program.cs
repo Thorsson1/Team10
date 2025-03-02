2cbfb339de86e89d11f3407ad5734e3c7ebaeb00
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-02 20:41:50 +0100

    RudTilføjelseOpgave15

--- Ruds_Opgave/RudOpgave15/Program.cs
+++ Ruds_Opgave/RudOpgave15/Program.cs
@@ -4,24 +4,17 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int sum = 0;
 
-            for (int i = 1; i <= 5; i++)
+            int i, sum = 0, n;
+            Console.Write("Skriv et tal her: ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i <= n; i++)
             {
-                sum += i;
+                sum = sum + i;
             }
-            Console.WriteLine("Summen af de første fem tal er: " + sum);
-            sum = 0;
+            Console.WriteLine("\nSummen af alle tal op til tallet du har skrevet er: " + sum);
 
-            foreach (int number in numbers)
-            {
-                sum += number;
-            }
-            Console.WriteLine("Den totale sum er: " + sum);
-        
-
-        Console.ReadKey();
+            Console.ReadKey();
         }
 
           
--- Ruds_Opgave/RudOpgave15/obj/Debug/net8.0/RudOpgave15.AssemblyInfo.cs
+++ Ruds_Opgave/RudOpgave15/obj/Debug/net8.0/RudOpgave15.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RudOpgave15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749b52955ee6836f66b685dbb5eaf2917fa139be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d83c78afaa28acdbd42dae7152585b5fd1bdeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RudOpgave15")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RudOpgave15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
