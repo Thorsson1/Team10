73d83c78afaa28acdbd42dae7152585b5fd1bdeb
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-02 20:31:34 +0100

    RudTilføjelseOpgave15

--- Ruds_Opgave/RudOpgave14/obj/Debug/net8.0/RudOpgave14.AssemblyInfo.cs
+++ Ruds_Opgave/RudOpgave14/obj/Debug/net8.0/RudOpgave14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RudOpgave14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6410e293f2bd9942f04cef1880342c4b3e48d8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749b52955ee6836f66b685dbb5eaf2917fa139be")]
 [assembly: System.Reflection.AssemblyProductAttribute("RudOpgave14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RudOpgave14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Ruds_Opgave/RudOpgave15/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace RudOpgave15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Summen af de første fem tal er: " + sum);
+            sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine("Den totale sum er: " + sum);
+        
+
+        Console.ReadKey();
+        }
+
+          
+        
+    }
+
+}
