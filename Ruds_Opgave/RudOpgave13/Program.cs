749b52955ee6836f66b685dbb5eaf2917fa139be
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-02 20:17:08 +0100

    RudTilføjelseOpgave14

---
+++ Ruds_Opgave/RudOpgave13/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RudOpgave13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            while (number <= 10)
+            {
+                Console.WriteLine("{0}", number);
+                number++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ruds_Opgave/RudOpgave14/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RudOpgave14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+
+            do
+            {
+                Console.WriteLine("{0}", number);
+                number++;
+            } while (number <= 10);
+
+
+            Console.ReadKey();
+        }
+    }
+}
