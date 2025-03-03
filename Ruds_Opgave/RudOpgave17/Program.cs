86d19d36ddacc982b8a94fa54735638fc006819f
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-03 19:57:05 +0100

    RudTilføjelseOpgave17

---
+++ Ruds_Opgave/RudOpgave17/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RudOpgave17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            double result;
+
+            Console.WriteLine("Enter in number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter in number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x, y);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+        static double Multiply(double x, double y)
+        {
+            double z = x * y;
+            return z;
+        }
+    }
+}
