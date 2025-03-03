5665ff64ce5f567718beb521f95d7a342d78e620
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-03 21:24:10 +0100

    RudTilføjelseOpgave21

---
+++ Ruds_Opgave/RudOpgave21/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RudOpgave21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubeSide = 4;
+            double cylinderRadius = 2.5;
+            double cylinderHeight = 5;
+            double pi = 3.1415926535897931;
+            Console.WriteLine("Kubens areal er: " + KubeUdregning(cubeSide));
+            Console.WriteLine("Cylinderens areal er: " + CylinderUdregning(cylinderRadius, cylinderHeight, pi));
+            Console.ReadKey();
+        }
+
+        static int KubeUdregning( int length)
+        {
+            return 6 * length * length;
+        }
+
+        static double CylinderUdregning(double radius, double height, double pi)
+        {
+            return 2 * pi * radius * (radius + height);
+        }
+    }
+}
