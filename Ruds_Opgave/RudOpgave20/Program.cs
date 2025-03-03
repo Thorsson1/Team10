748e17557e8842361f01aa2ecf6fc124dfdc00e4
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-03 21:15:00 +0100

    RudTilføjelseOpgave20

---
+++ Ruds_Opgave/RudOpgave20/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RudOpgave20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firkantSide = 5;
+            double cirkelRadius = 5.5;
+            double pi = 3.1415926535897931;
+            Console.WriteLine("Arealet af firkanten er: " + FirkantUdregning(firkantSide));
+            Console.WriteLine("Arealet af cirklen er: " + Math.Ceiling(CirkelUdregning(cirkelRadius, pi)));
+            Console.ReadKey();
+
+            static int FirkantUdregning(int length)
+            {
+                return length * length;
+            }
+            static double CirkelUdregning(double radius, double pi)
+            {
+                return pi * radius * radius;
+            }
+        }
+    }
+}
