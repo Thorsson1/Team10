d5c954d08d34601266d5e802fc9e9a6b7a0726b0
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-03 19:28:59 +0100

    RudTilføjelseOpgave16

---
+++ Ruds_Opgave/RudOpgave16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RudOpgave16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Skriv et tal her: ");
+            int n = int.Parse(Console.ReadLine());
+            int fact = 1;
+            for (int x = 1; x <= n; x++)
+            {
+                fact *= x;
+            }
+            Console.WriteLine("\nFakulteten af tallet du har skrevet er: " + fact);
+
+            Console.ReadKey();
+        }
+    }
+}
